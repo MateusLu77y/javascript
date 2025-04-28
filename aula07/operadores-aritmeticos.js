089159894951d9fda999496ae7bd2ae3ca3cb99a
Lu77y <mateusdasilvavilaca@gmail.com>
2025-04-28 10:42:17 -0300

    Operadores part 1

---
+++ aula07/operadores-aritmeticos.js
@@ -0,0 +1,23 @@
+ /* Temos vários operadores aritméticos veja:
+        Adição (+)
+        Subtração (-)
+        Multiplicação (*)
+        Divisão (/)
+        Módulo (%) (resto da divisão)
+        Exponencial (**) (pontencialização)
+        Incrementar (++) (adicionar uma unidade)
+        Decrementar (--) (subtrair uma unidade)
+*/
+        //Exemplos de operaçãoes:
+        
+        let n1 = 4 //Variável n1 recebe o valor de 4.
+        let n2 = 5 //Vartiável n2 recebe o valor de 5.
+
+        let adição = n1 + n2 // 4 + 5 = 9
+        let subtração = n1 - n2  // 4 - 5 = -1 
+        let multiplicação = n1 * n2 // 4 x 5 = 20
+        let divisão = n1 / n2 // 4 / 5 = 0.8
+        let modulo = n1 % n2 // 4 / 5 = 4
+        let exponencial = n1 ** n2 // 4 ^ 5 = 1024
+        let incrementar = n1 ++ // 4 + 1 = 5
+        let decrementar = n1 -- // 4 - 1 = 3
---
+++ aula07/operadores-de-atribuição.js
@@ -0,0 +1,12 @@
+/* Operadores de atribuição, são usados para atribuir valores a variáveis */
+
+// Exemplo de operadores de atribuição:
+
+let n1 = 5 // operador de recebe (=)
+n1 += 5 // n1 = n1 + 5 (forma normal)
+n1 -= 5 // n1 = n1 - 5 (forma normal)
+n1 *= 5 // n1 = n1 * 5 (forma normal)
+n1 /= 5 // n1 = n1 / 5 (forma normal)
+n1 %= 5 // n1 = n1 % 5 (forma normal)
+n1 **= 5 // n1 = n1 ** 5 (forma normal)
+
