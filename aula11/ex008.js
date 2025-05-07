25ba3d5d0a8778f64233adb0573145a1e8eac852
Mateus Silva <mateusdasilvavilaca@gmail.com>
2025-05-07 17:36:13 -0300

    Novos exercícios

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+//Teste de velocidade
+let vel = 45
+
+console.log(`A sua velocidade atual é de ${vel}km/h`)
+if (vel > 60) {
+    console.log("Você ultrapassou o excesso de velocidade da via, você foi MULTADO!")
+}
+
+console.log("Dirija sempre com o cinto de segurança!")
---
+++ aula11/ex009.js
@@ -0,0 +1,9 @@
+//De onde você é?
+let pais = 'França'
+
+if (pais != 'Brasil') {
+    console.log("Você é estrangeiro!")
+}
+else {
+    console.log("Você é brasileiro!")
+}
