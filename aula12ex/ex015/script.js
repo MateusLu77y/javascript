858255a91e12481810abdf6749be9c3965b17104
Mateus Silva <mateusdasilvavilaca@gmail.com>
2025-05-09 16:38:11 -0300

    Finalizando Verificador de idade!

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,18 +1,60 @@
 function validar() {
-    let agora = new Date()
-    let anoAtual = agora.getFullYear()
+    let agora = new Date() //pegando a data atual
+    let anoAtual = agora.getFullYear() //pegando o ano, fullyear 4 dígitos!
+
     //Selecionando:
-    let anoNas = document.getElementById('textano')
-    let msg = document.getElementById('msg')
-    let img = document.getElementById('img')
+    let anoNas = document.getElementById('textano') //Ano de nascimento
+    let msg = document.getElementById('msg') //Mensagem da idade
+    let img = document.getElementById('img') //Imagem da pessoa
 
-    let sexom = document.getElementById('sexm')
-    let sexf = document.getElementById('sexf')
-
-    if (anoNas.value.length == 0 || anoNas > anoAtual) {
+    //Válidando o anoNas:
+    if (anoNas.value.length == 0 || Number(anoNas.value) > anoAtual) {
         window.alert("[ERRO] - Ano inválido, digite outro ano!")
-    } else{alert("Tudo Ok")}
+    } else {
+        let sexo = document.getElementsByName('gsex')
+        let idade = anoAtual - anoNas.value
+        let genero = ''
 
+        if (sexo[0].checked) {
+            genero = 'Homem'
+
+            //Válidação da imagem do individuo
+            if (idade >= 0 && idade <= 10) {
+                img.src = 'imagens/bebe-m.png'
+                //Criança
+            } else if (idade <= 21) {
+                img.src = 'imagens/adolescente-m.png'
+                //jovem
+            } else if (idade <= 50) {
+                img.src = 'imagens/adulto-m.png'
+                //Adulto
+            } else if (idade >= 51)
+                img.src = 'imagens/idoso-m.png'
+            //Idoso
+
+
+
+        } else if (sexo[1].checked) {
+            genero = 'Mulher'
+
+            //Válidação da imagem do individuo
+            if (idade >= 0 && idade <= 10) {
+                img.src = 'imagens/bebe-f.png'
+                //Criança
+            } else if (idade <= 21) {
+                img.src = 'imagens/adolescente-f.png'
+                //jovem
+            } else if (idade <= 50) {
+                img.src = 'imagens/adulto-f.png'
+                //Adulto
+            } else if (idade >= 51)
+                img.src = 'imagens/idoso-f.png'
+            //Idoso
+
+        }
+        msg.style.textAlign = 'center'
+        msg.innerHTML = (`Detectamos ${genero} com ${idade} anos`)
+    }
 
 
     console.log('Funcionando!')
