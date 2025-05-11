c8ef468c1b2cdf7a6d0ecd6a9679e96cf4cd8123
Mateus Silva <mateusdasilvavilaca@gmail.com>
2025-05-11 13:07:00 -0300

    Create ambiente.js

---
+++ aula14/ambiente.js
@@ -0,0 +1,21 @@
+let n = 0
+
+//while:
+
+// while (n < 9) {
+//     n += 1
+//     console.log(`O número ${n} é menor que 10`)
+// }
+
+//do while:
+
+// do {
+//     n += 1
+//     console.log(`O número ${n} é menor que 10`)
+// } while (n < 9)
+
+//for:
+
+for (n = 0; n < 10; n += 1) {
+    console.log(`O número ${n} é menor que 10`)
+}
