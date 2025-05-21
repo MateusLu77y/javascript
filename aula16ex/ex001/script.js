8d27ff282a5f2767d403a5a7dd893cad6f1cb85b
Mateus Silva <mateusdasilvavilaca@gmail.com>
2025-05-21 19:01:34 -0300

    Adicionado!
    Novo botão de finalizar, que mostra os dados formatados! (em desenvolvimento)

--- aula16ex/ex001/script.js
+++ aula16ex/ex001/script.js
@@ -5,6 +5,7 @@ function adicionar() {
     //Selecionando:
     let numStr = document.getElementById('numero').value
     let num = Number(numStr)
+    let tab = document.getElementById('tabela')
 
     //Validação:
     if (num == '') {
@@ -18,8 +19,29 @@ function adicionar() {
             listNum.push(num)
             console.log(`O número ${num} foi adicionado a lista`)
             console.log(`Lista atualizada ${listNum}`)
+
+            //Criação de Elemento:
+            let opcao = document.createElement('option')
+            opcao.innerHTML = `O valor ${num} foi adicionado.`
+            tab.appendChild(opcao)
+
         } else {
             alert(`O número ${num} já foi adicionado!`)
         }
     }
+
 }
+
+function dados() {
+    //Criação de Elemento:
+    let res = document.getElementById('resd')
+    let result = document.createElement('p')
+
+    result.innerHTML += `Ao todo temos ${listNum.length}  números cadastrados. <br><br>`
+    result.innerHTML += `O maior valor informado foi.<br><br>`
+    result.innerHTML += `O menor valor informado foi .<br><br>`
+    result.innerHTML += `A média dos valores digítados é .<br><br>`
+    res.appendChild(result)
+
+}
+
